f14f7383afbffe24a4ededd49d1cf6f3a56dd89c
simondeu <simondeuten@gmail.com>
2023-03-13 13:02:24 +0100

    R

---
+++ RScript.R
@@ -0,0 +1,15 @@
+
+#Importing Data
+
+library("readxl")
+Data1 = read_excel("Excelusssy.xlsx", sheet="Problem")
+Data2 = read_excel("Excelusssy.xlsx", sheet="Problem2")
+Data3 = read_excel("Excelusssy.xlsx", sheet="Problem3")
+Data4 = read_excel("Excelusssy.xlsx", sheet="Problem4")
+Data5 = read_excel("Excelusssy.xlsx", sheet="Problem5")
+Data6 = read_excel("Excelusssy.xlsx", sheet="Problem6")
+
+
+
+
+
